a701a88fa9b65ea44d9370940c70b08f028f60a4
carlos <ce4555678@gmail.com>
2021-03-05 19:05:09 -0300

    teste player cloudflare

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 // import { Button, Container } from 'react-bootstrap';
-import Player from '../components/Player'
-import Layout from '../components/Layout'
+import { Stream } from "@cloudflare/stream-react";
 export default function Home() {
   const url = "5d5bc37ffcf54c9b82e996823bffbb81"
   return (
     <Layout title="home">
-      <Player id="player" file="https://videodelivery.net/5d5bc37ffcf54c9b82e996823bffbb81/manifest/video.mpd or https://videodelivery.net/5d5bc37ffcf54c9b82e996823bffbb81/manifest/video.m3u8"/>
       <h3  className="text-white text-center">Convenção das Bruxas</h3>
-      <div  id="player"></div>
+      <Stream src={url}/>
     </Layout>
   )
 }
