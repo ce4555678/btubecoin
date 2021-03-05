05ded9d609ecafcc0b56a52214fe8871539134fe
carlos <ce4555678@gmail.com>
2021-03-05 18:57:31 -0300

    teste player

--- pages/send-video.js
+++ pages/send-video.js
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import Layout from '../components/Layout'
 
 export default function SendVideo() {
     return (
         <Layout title="Send Video">
-            
+            <Link href="/">
+                <h1 className="text-white">incio</h1>
+            </Link>
         </Layout>
     )
 }
